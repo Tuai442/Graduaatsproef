4f44723925b32be9e16554e57c6c194b18a12669
Tuai442 <tuur.vanholen@outlook.com>
2022-10-13 11:22:53 +0100

    controller

--- Code/BezoekerRegistratie/BezoekerApp/obj/Debug/net6.0-windows/App.g.cs
+++ Code/BezoekerRegistratie/BezoekerApp/obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace BezoekerApp {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace BezoekerApp {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
         public static void Main() {
             BezoekerApp.App app = new BezoekerApp.App();
             app.InitializeComponent();
