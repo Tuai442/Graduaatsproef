b083086f91a4ea7331bd1880c8a7011491f175ce
SorenKeytsman <soren.keytsman@student.hogent.be>
2022-10-14 13:20:52 +0200

    Database

--- Code/BezoekerRegistratie/Databank/Bezoeker.sql
+++ Code/BezoekerRegistratie/Databank/Bezoeker.sql
@@ -4,6 +4,6 @@
     [voornaam] VARCHAR(50) NOT NULL, 
     [achternaam] VARCHAR(50) NOT NULL, 
     [email] VARCHAR(50) NOT NULL, 
-    [bedrijf] VARCHAR(50) NOT NULL, 
+    [bedrijfId] INT NOT NULL, 
     [nummerplaat] VARCHAR(50) NOT NULL
 )
