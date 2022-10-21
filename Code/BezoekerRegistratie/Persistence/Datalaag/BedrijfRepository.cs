0ad7af070b2d0432f850d24bd2b2a47e07ac79ec
SorenKeytsman <soren.keytsman@student.hogent.be>
2022-10-21 15:15:50 +0200

    Repos

--- Code/BezoekerRegistratie/Persistence/Datalaag/BedrijfRepository.cs
+++ Code/BezoekerRegistratie/Persistence/Datalaag/BedrijfRepository.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Controller.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -19,6 +21,39 @@ namespace Persistence.Datalaag
         {
             SqlConnection connection = new SqlConnection(connectionString);
             return connection;
+        }
+        public List<Bedrijf> GeefAlleBedrijven()
+        {
+            string query = "SELECT * from dbo.Bedrijf";
+            SqlConnection conn = GetConnection();
+            using (SqlCommand command = new SqlCommand(query, conn))
+            {
+                try
+                {
+                    List<Bedrijf> bedrijven = new List<Bedrijf>();
+                    conn.Open();
+                    IDataReader dataReader = command.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        Bedrijf bedrijf = new Bedrijf(/*(int)dataReader["bedrijfId"],*/ (string)dataReader["naam"], (string)dataReader["btwNummer"], (string)dataReader["adres"], (string)dataReader["telefoon"], (string)dataReader["email"]);
+                        bedrijven.Add(bedrijf);
+                    }
+                    dataReader.Close();
+                    foreach (Bedrijf bedrijf in bedrijven)
+                    {
+                        Console.WriteLine(bedrijf);
+                    }
+                    return bedrijven;
+                }
+                catch (Exception ex)
+                {
+                    throw new BedrijfException("Geef bedrijven is niet gelukt.", ex);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
         }
     }
 }
--- Code/BezoekerRegistratie/Persistence/Datalaag/werknemerRepository.cs
+++ Code/BezoekerRegistratie/Persistence/Datalaag/werknemerRepository.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Controller.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,6 +23,41 @@ namespace Persistence.Datalaag
             SqlConnection connection = new SqlConnection(connectionString);
             return connection;
         }
+
+
+        public List<Werknemer> GeefAlleWerknemer()
+        {
+            string query = "SELECT * FROM dbo.Werknemer INNER JOIN Bedrijf ON dbo.Werknemer_bedrijfId = dbo.Bedrijf_bedrijfId";
+            SqlConnection conn = GetConnection();
+            using (SqlCommand command = new SqlCommand(query, conn))
+            {
+                try
+                {
+                    List<Werknemer> werknemers = new List<Werknemer>();
+                    conn.Open();
+                    IDataReader dataReader = command.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        Werknemer werknemer = new Werknemer(/*(int)dataReader["WerknemerId"],*/ (string)dataReader["voornaam"], (string)dataReader["achternaam"], (string)dataReader["email"], (string)dataReader["functie"], (string)dataReader["bedrijfNaam"]);
+                        werknemers.Add(werknemer);
+                    }
+                    dataReader.Close();
+                    foreach (Werknemer werknemer in werknemers)
+                    {
+                        Console.WriteLine(werknemer);
+                    }
+                    return werknemers;
+                }
+                catch (Exception ex)
+                {
+                    throw new WerknemerException("Geef werknemers is niet gelukt.", ex);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
     }
-    }
+}
 
