c82eb4953083a67b3b42f63a27733f1b4e8bbe49
SorenKeytsman <soren.keytsman@student.hogent.be>
2022-10-24 16:23:48 +0200

    Verwijder en toevoegevn bedrijf

--- Code/BezoekerRegistratie/Persistence/Datalaag/BedrijfRepository.cs
+++ Code/BezoekerRegistratie/Persistence/Datalaag/BedrijfRepository.cs
@@ -44,7 +44,7 @@ namespace Persistence.Datalaag
                         string adres = (string)dataReader["Adres"];
                         string tel = (string)dataReader["Telefoon"];
                         Bedrijf bedrijf = new Bedrijf(id, naam, btw, adres, tel, email);
-                        
+
                         bedrijven.Add(bedrijf);
                     }
 
@@ -69,10 +69,65 @@ namespace Persistence.Datalaag
 
         public void VoegNieuwBedrijfToe(Bedrijf bedrijf)
         {
-            throw new NotImplementedException();
+            // public Bedrijf(int id, string naam, string btw, string adres, string telefoon, string email)
+            string query = "INSERT INTO dbo.Bedrijf (naam,btwNummer,adres,telefoon,email) VALUES(@naam,@btwNummer,@adres,@telefoon,@email)";
+            SqlConnection conn = GetConnection();
+            using (SqlCommand command = new SqlCommand(query, conn))
+            {
+                try
+                {
+                    conn.Open();
+                    command.Parameters.Add(new SqlParameter("@naam", SqlDbType.NVarChar));
+                    command.Parameters.Add(new SqlParameter("@btwNummer", SqlDbType.NVarChar));
+                    command.Parameters.Add(new SqlParameter("@adres", SqlDbType.NVarChar));
+                    command.Parameters.Add(new SqlParameter("@telefoon", SqlDbType.Int));
+                    command.Parameters.Add(new SqlParameter("@email", SqlDbType.NVarChar));
+                    command.Parameters["@naam"].Value = bedrijf.Naam;
+                    command.Parameters["@btwNummer"].Value = bedrijf.Btw;
+                    command.Parameters["@adres"].Value = bedrijf.Adres;
+                    command.Parameters["@telefoon"].Value = bedrijf.Telefoon;
+                    command.Parameters["@email"].Value = bedrijf.Email;
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    BedrijfException be = new BedrijfException("Bedrijf toevoegen is niet gelukt", e);
+                    be.Data.Add("Bedrijf:", bedrijf);
+                    throw be;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+        public void VerwijderBedrijf(Bedrijf bedrijf)
+        {
+            string query = "DELETE FROM dbo.Bedrijf WHERE id=@id";
+            SqlConnection conn = GetConnection();
+            using (SqlCommand command = new SqlCommand(query, conn))
+            {
+                try
+                {
+                    conn.Open();
+                    command.Parameters.Add(new SqlParameter("@id", SqlDbType.Int));
+                    command.Parameters["@id"].Value = bedrijf.Id;
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    BedrijfException be = new BedrijfException("Bedrijf verwijderen is niet gelukt", e);
+                    be.Data.Add("Bedrijf:", bedrijf);
+                    throw be;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
         }
 
-        
+
 
         public List<Bedrijf> ZoekBedrijfOp(string zoekText)
         {
@@ -85,6 +140,6 @@ namespace Persistence.Datalaag
             return GeefBedrijfOpId(id);
         }
 
-       
+
     }
 }
