f6ddf7d8762dc73bb5acb631138e6c31f927fc02
Tomokari123 <92168748+Tomokari123@users.noreply.github.com>
2022-11-09 20:41:54 +0100

    commit die ding

--- Code/BezoekerRegistratie/BezoekerApp/obj/Debug/net6.0-windows/Paginas/TestNav.g.i.cs
+++ Code/BezoekerRegistratie/BezoekerApp/obj/Debug/net6.0-windows/Paginas/TestNav.g.i.cs
@@ -53,7 +53,7 @@ namespace BezoekerApp.Paginas {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/BezoekerApp;component/paginas/testnav.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/BezoekerApp;V1.0.0.0;component/paginas/testnav.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Paginas\TestNav.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
