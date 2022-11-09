baa44597a00c46fea98c29de86021486c81741fb
Tuai442 <tuur.vanholen@outlook.com>
2022-11-09 16:52:27 +0100

    update

--- Test Data/test.py
+++ Test Data/test.py
@@ -2,12 +2,23 @@ import pandas as pd
 
 bedrijven = pd.read_csv("dummydata_bedrijven.csv")
 afspraken = pd.read_csv("dummydata_afspraken.csv")
-print(bedrijven)
-print(afspraken)
+bezoekers = pd.read_csv("dummydata_bezoeker.csv")
+werknemers = pd.read_csv("dummydata_werknemers.csv")
 
-merger = pd.DataFrame(pd.merge(afspraken, bedrijven, on="BedrijfId")["BedrijfId"])
+niew_afspr = pd.DataFrame()
 
-print(type(merger))
-print(merger.columns)
-bedrijfNaam = merger[merger["BedrijfId"] == 1]
-print(bedrijfNaam)
+niew_afspr["BezoekerVoornaam"] = afspraken["BezoekerVoornaam"]
+niew_afspr["BezoekerAchternaam"] = afspraken["BezoekerAchternaam"]
+
+niew_afspr["WerknemerVoornaam"] = werknemers["BezoekerAchtenaam"]
+niew_afspr["WerknemerAchternaam"] = werknemers["BezoekerVoornaam"]
+
+for row in bezoekers.iteritems():
+    niew_afspr["BezoekerVoornaam"] = row["voornaam"]
+    niew_afspr["BezoekerEmail"] = f"{row['voornaam']}.{row['achternaam']}@email.com"
+
+for row in werknemers.iterrows():
+    niew_afspr["WerknemerEmail"] = f"{row['voornaam']}.{row['achternaam']}@email.com"
+
+
+print(niew_afspr)
