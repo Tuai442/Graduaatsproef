058915d2114167f846f6beffb55f9be5eaefd960
Tuai442 <tuur.vanholen@outlook.com>
2023-01-11 22:18:44 +0100

    final

--- Test Data/genenereer_query.py
+++ Test Data/genenereer_query.py
@@ -22,50 +22,31 @@ with open(NEW_QUERY, "w") as new_file:
     new_file.write("\n")
     for row in bedrijven.iterrows():
         r = row[1]
-        insert_query = f"INSERT INTO Bedrijf (naam, btwNummer, email, adres, telefoon, actief) VALUES ('{r[1]}', '{r[2]}', '{r[3]}', '{r[4]}', '{r[5]}', '1');"
+        insert_query = f"INSERT INTO Bedrijf (Naam, btwNummer, Email, Adres, Telefoon) VALUES ('{r[1]}', 'BE 0202.239.951', '{r[3]}', '{r[4]}', '0487788759');"
 
         new_file.write(insert_query)
         new_file.write("\n")
 
     new_file.write("\n")
     new_file.write("\n")
-    # Werknemers
     for row in werknemers.iterrows():
         r = row[1]
-        vn = r[1].replace(" ", "")
-        an = r[2].replace(" ", "")
-        email = f"{vn}.{an}@email.com".replace(" ", "")
-        insert_query = f"INSERT INTO Werknemer (voornaam, achternaam, email, functie, bedrijfId, actief) VALUES ('{r[1]}', '{r[2]}', '{email}', '{r[4]}', '{r[5]}', '1');"
+        email = f"{r[1]}.{r[2]}@email.com".replace(" ", "")
+        insert_query = f"INSERT INTO Werknemer (Voornaam, Achternaam, Email, Functie, BedrijfId) VALUES ('{r[1]}', '{r[2]}', '{email}', '{r[4]}', '{r[5]}');"
 
         new_file.write(insert_query)
         new_file.write("\n")
 
     new_file.write("\n")
     new_file.write("\n")
-    random_bezoeker_bedrijven = ["Remotive", "Oleon", "Vpk", "Stora Enzo", "Ineo Fenol", "LLh", "SidMar", "Aligist Bruggeman"]
-    # Bezoekers
-    for row in werknemers.iterrows():
-        r = row[1]
-        vn = r[1].replace(" ", "")
-        an = r[2].replace(" ", "")
-        email = f"{vn}.{an}@email.com"
-        bedrijf = random_bezoeker_bedrijven[random.randint(0, len(random_bezoeker_bedrijven) - 1)]
-        insert_query = f"INSERT INTO Bezoeker (voornaam, achternaam, email, bedrijf, aanwezig, nummerplaat) VALUES ('{r[1]}', '{r[2]}', '{email}', '{bedrijf}', '0', '');"
-
-        new_file.write(insert_query)
-        new_file.write("\n")
-
-    new_file.write("\n")
-    new_file.write("\n")
-    bezoekers_aantal = len(werknemers) - 1
     for row in afspraken.iterrows():
         r = row[1]
-        vn = r[1].replace(" ", "")
-        an = r[2].replace(" ", "")
-        email = f"{vn}.{an}@email.com".replace(" ", "")
-        random_bezoekers_id = random.randint(1, bezoekers_aantal)
-        insert_query = f"INSERT INTO Afspraak (startTijd, eindTijd, werknemerId, bezoekerId )" \
-                       f" VALUES ('{r[4]}', '{r[5]}', '{r[7]}', '{random_bezoekers_id}');"
+        randomIndex = random.randint(0, len(bedrijven))
+        bedrijfNaamArr = bedrijven[bedrijven["BedrijfId"] == randomIndex]["Naam"]
+        bedrijfNaam = ""
+
+        print(bedrijfNaam)
+        insert_query = f"INSERT INTO Afspraak ( Email, StartTijd, EindTijd, WerknemerId ) VALUES ('{r[1]}', '{r[2]}', '{bedrijfNaam}', '{r[3]}', '{r[4]}', '{r[5]}', '{r[7]}');"
 
         new_file.write(insert_query)
         new_file.write("\n")
